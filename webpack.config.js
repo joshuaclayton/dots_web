b189b7de95ed355b0c649b2773cd9c67f3b49aab
Joshua Clayton <jclayton@thoughtbot.com>
2015-12-19 14:08:40 -0500

    Brunch -> Webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: "./web/static/js/app.js",
+  output: {
+    path: "./priv/static/js",
+    filename: "app.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
